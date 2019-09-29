faf31f2d47584f81ca4710d6bfc1b089e51d5cc7
Abdul khadar <atoao@users.noreply.github.com>
2019-09-28 23:26:24 -0400

    Create asciiToUtf.lua

---
+++ asciiToUtf.lua
@@ -0,0 +1,103 @@
+
+local pubic = {}
+
+-- returns the char and the index of the next character in the stream s 
+-- also doubles as a UTF-8 character validator
+
+function public.getutf8char (s, i)
+	-- argument defaults
+	i = i or 1
+
+	-- argument checking
+	if type(s) ~= "string" then
+		error("bad argument #1 to 'utf8charbytes' (string expected, got ".. type(s).. ")")
+	end
+	if type(i) ~= "number" then
+		error("bad argument #2 to 'utf8charbytes' (number expected, got ".. type(i).. ")")
+	end
+
+	local c = string.byte(s, i)
+
+	-- determine bytes needed for character, based on RFC 3629
+	-- validate byte 1
+	if c > 0 and c <= 127 then
+		-- UTF8-1
+		return string.char(c), i+1
+
+	elseif c >= 194 and c <= 223 then
+		-- UTF8-2
+		local c2 = string.byte(s, i + 1)
+
+		if not c2 then
+			error("UTF-8 string terminated early")
+		end
+
+		-- validate byte 2
+		if c2 < 128 or c2 > 191 then
+			error("Invalid UTF-8 character")
+		end
+
+		return string.char(c, c2), i+2
+
+	elseif c >= 224 and c <= 239 then
+		-- UTF8-3
+		local c2 = string.byte(s, i + 1)
+		local c3 = string.byte(s, i + 2)
+
+		if not c2 or not c3 then
+			error("UTF-8 string terminated early")
+		end
+
+		-- validate byte 2
+		if c == 224 and (c2 < 160 or c2 > 191) then
+			error("Invalid UTF-8 character")
+		elseif c == 237 and (c2 < 128 or c2 > 159) then
+			error("Invalid UTF-8 character")
+		elseif c2 < 128 or c2 > 191 then
+			error("Invalid UTF-8 character")
+		end
+
+		-- validate byte 3
+		if c3 < 128 or c3 > 191 then
+			error("Invalid UTF-8 character")
+		end
+
+		return string.char(c, c2, c3), i+3
+
+	elseif c >= 240 and c <= 244 then
+		-- UTF8-4
+		local c2 = string.byte(s, i + 1)
+		local c3 = string.byte(s, i + 2)
+		local c4 = string.byte(s, i + 3)
+
+		if not c2 or not c3 or not c4 then
+			error("UTF-8 string terminated early")
+		end
+
+		-- validate byte 2
+		if c == 240 and (c2 < 144 or c2 > 191) then
+			error("Invalid UTF-8 character")
+		elseif c == 244 and (c2 < 128 or c2 > 143) then
+			error("Invalid UTF-8 character")
+		elseif c2 < 128 or c2 > 191 then
+			error("Invalid UTF-8 character")
+		end
+
+		-- validate byte 3
+		if c3 < 128 or c3 > 191 then
+			error("Invalid UTF-8 character")
+		end
+
+		-- validate byte 4
+		if c4 < 128 or c4 > 191 then
+			error("Invalid UTF-8 character")
+		end
+
+		return string.char(c, c2, c3, c4), i+4
+
+	else
+		error("Invalid UTF-8 character")
+	end
+end
+
+return public
